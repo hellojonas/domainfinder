4bfdbdfe599b60d9569a4af7f513c14e111f9758
Hello Jonas <hellojonashp@gmail.com>
2022-08-27 22:52:05 +0200

    feat: check if domain is available

---
+++ cmd/available/available.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	marks := map[bool]string{
+		true:  "✔",
+		false: "✘",
+	}
+	sc := bufio.NewScanner(os.Stdin)
+
+	for sc.Scan() {
+		t := sc.Text()
+		ok, err := exists(t)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		fmt.Fprintf(os.Stdout, "%-32s %-2v\n", t, marks[!ok])
+	}
+}
+
+func exists(domain string) (bool, error) {
+	whois := "whois.verisign-grs.com" // supports .com, .net & .edu tlds
+	match := "no match for"
+	conn, err := net.Dial("tcp", whois+":43")
+
+	if err != nil {
+		return false, fmt.Errorf("Failed to open connection on '%s'. %s", whois, err.Error())
+	}
+
+	defer conn.Close()
+	_, err = conn.Write([]byte(domain + "rn"))
+
+	if err != nil {
+		return false, fmt.Errorf("failed to write data to whois server. %s", err.Error())
+	}
+
+	sc := bufio.NewScanner(conn)
+
+	for sc.Scan() {
+		t := sc.Text()
+
+		if strings.Contains(strings.ToLower(t), match) {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
